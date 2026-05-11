34a90c8e0c70af34fbaa274fd7222b33b2a82c64
Jiri Jaburek <comps@nomail.dom>
2026-05-11 11:28:51 +0200

    add BeakerlibExecutor tests for 'rlImport --all'
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

---
+++ tests/executor/beakerlib/fmf_trees/libraries/rlimport_all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+rlJournalStart
+    rlPhaseStartTest
+        rlRun "rlImport --all"
+        rlRun "yumlibIsDnf5" 0,1
+    rlPhaseEnd
+rlJournalPrintText
+rlJournalEnd

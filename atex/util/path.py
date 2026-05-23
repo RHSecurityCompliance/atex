6aab31c4a60ff315fb59c33ca1473067393a037a
Jiri Jaburek <comps@nomail.dom>
2026-05-23 09:54:30 +0200

    sanitize ASCII 0x00-0x1F in paths (mostly control chars)
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- atex/util/path.py
+++ atex/util/path.py
@@ -1,3 +1,4 @@
+import re
 from pathlib import PurePath
 
 
@@ -8,6 +9,8 @@ def normalize_path(src):
 
     Always returns a relative path.
     """
+    # replace control characters (null, newline, tab, etc.) with underscores
+    src = re.sub(r"[\x00-\x1f\x7f-\x9f]", "_", str(src))
     parts = (
         part for part in PurePath(src).parts
         if part not in (".","..") and "/" not in part

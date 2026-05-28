56e686b811ce149d8693b567396ca910211b7e41
Jiri Jaburek <comps@nomail.dom>
2026-05-28 12:22:35 +0200

    improve LocalProvisioner a bit, consistent with others
    Signed-off-by: Jiri Jaburek <comps@nomail.dom>

--- atex/provisioner/local/local.py
+++ atex/provisioner/local/local.py
@@ -1,7 +1,7 @@
 import threading
 
 from ... import connection, util
-from .. import Provisioner, Remote
+from .. import Provisioner, ProvisionerError, Remote
 
 _get_logger = util.get_loggers("atex.provisioner.local")
 
@@ -26,8 +26,10 @@ class LocalRemote(Remote, connection.local.LocalConnection):
                 return
             else:
                 self._release_called = True
-        self.disconnect()
-        self.release_hook(self)
+        try:
+            self.disconnect()
+        finally:
+            self.release_hook(self)
 
     def __str__(self):
         class_name = self.__class__.__name__
@@ -41,58 +43,58 @@ class LocalProvisioner(Provisioner):
     """
 
     def __init__(self, **kwargs):
-        self._lock = threading.RLock()
+        self._lock = threading.Condition()
         self.logger = _get_logger()
-        self._remotes = []
+        self._remotes = set()
         self._requested = 0
-        self._cond = threading.Condition()
-        self._started = False
+        self._stopped = True
         self.kwargs = kwargs
 
     def start(self):
         self.logger.debug(f"starting: {self}")
-        self._started = True
+        self._stopped = False
 
     def stop(self):
         self.logger.debug(f"stopping: {self}")
-        with self._cond:
-            self._started = False
-            self._cond.notify_all()
         with self._lock:
+            self._stopped = True
+            self._lock.notify_all()
             while self._remotes:
                 self._remotes.pop().release()
 
     def provision(self, count=1):
-        assert count >= 0
+        if self._stopped:
+            raise ProvisionerError("the provisioner is stopped")
+
         self.logger.debug(f"provisioning {count}")
-        with self._cond:
+        with self._lock:
             self._requested += count
-            self._cond.notify(count)
+            self._lock.notify(count)
 
     def get_remote(self, block=True):
-        with self._cond:
+        with self._lock:
             if block:
-                self._cond.wait_for(lambda: self._requested > 0 or not self._started)
-                if not self._started:
-                    return None
-            elif self._requested <= 0:
+                self._lock.wait_for(lambda: self._requested > 0 or self._stopped)
+
+            if self._stopped:
+                raise ProvisionerError("the provisioner is stopped")
+
+            if self._requested <= 0:
                 return None
+
             self._requested -= 1
 
-        def release_hook(remote):
-            self.logger.debug(f"releasing {remote}")
-            with self._lock:
-                try:
-                    self._remotes.remove(remote)
-                except ValueError:
-                    pass
+            def release_hook(remote):
+                self.logger.debug(f"releasing {remote}")
+                with self._lock:
+                    self._remotes.discard(remote)
 
-        remote = LocalRemote(release_hook=release_hook, **self.kwargs)
-        self._remotes.append(remote)
-        return remote
+            remote = LocalRemote(release_hook=release_hook, **self.kwargs)
+            self._remotes.add(remote)
+            return remote
 
     def clear(self):
-        with self._cond:
+        with self._lock:
             self._requested = 0
 
     def __str__(self):
